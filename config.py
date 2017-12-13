9e3ae8491dc602bf4609a2a48fe81b501803d14a
root <root@snake.long.cn>
2017-12-13 02:34:26 -0500

    Chapter 8: Authentication blueprint(8b)

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ class Config:
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
     FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
-    FLASKY_ADMIN = os.environ.get['FLASKY_ADMIN']
+    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
